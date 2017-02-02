96299b4316911d24fb086ffca27230452a7cdeeb
Samuel PH Sinaga & Triyan Arief Wibowo <pair+samuelphsinaga.triyanarief@hacktiv8.com>
2017-02-02 17:21:56 +0700

    baru selesai papan

--- dice.js
+++ dice.js
@@ -2,11 +2,14 @@
 
 class Dice {
   constructor() {
-
+    this.kocok = 0
   }
   roll() {
-    
+    var kocok = Math.floor((Math.random() * 6) + 1);
+    return kocok
   }
 }
+// let dice = new Dice()
+// console.log(dice.roll());
 
 export default Dice
--- js_racer.js
+++ js_racer.js
@@ -3,27 +3,47 @@
 import Dice from "./dice.js"
 
 class JSRacer {
-  constructor(players, length, sides) {
-
+  constructor(players, length) {
+    this.player = players
+    this.playerData =  [{name : "a", pos : 0}, {name :"b", pos :0},{name :"c", pos :0}]
+    this.length = length
+    this.pemenang = false;
   }
   print_board() {
-
+    for(var i=0; i<this.player; i++){
+        this.print_line(this.playerData[i].name,0)
+    }
   }
   print_line(player, pos) {
-
+    let arr = []
+    for(var i=0; i<this.length; i++){
+      if(pos == i){
+        arr.push(player)
+      }else{
+        arr.push(" ")
+      }
+    }
+    console.log(arr.join("|"))
   }
-  advanced_player(player) {
+  advanced_player(player){
 
-  }
+    while(this.pemenang === false){
+      return 
+    }
+ }
   finished() {
 
-  }
+ }
   winner() {
 
-  }
+ }
   reset_board() {
     console.log("\x1B[2J")
   }
 }
+var test = new JSRacer(3,30)
+
+// test.print_line('a',0)
+test.print_board()
 
 export default JSRacer
