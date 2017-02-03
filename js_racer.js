e70a88989c48123a0c4f902819789276b9291a2c
samuelphsinaga <samuelphsinaga@gmail.com>
2017-02-03 10:58:25 +0700

    js-racer done

--- js_racer.js
+++ js_racer.js
@@ -8,6 +8,8 @@ class JSRacer {
     this.playerData =  [{name : "a", pos : 0}, {name :"b", pos :0},{name :"c", pos :0}]
     this.length = length
     this.jalan = 1;
+    this.finish = false
+    this.pemenang = null
   }
 
   //menampilkan posisi start
@@ -32,18 +34,40 @@ class JSRacer {
 
   //proses berjalan permainan
   advanced_player(player){
+
     this.print_board()
 
-    this.playerData[0].pos += Dice.roll()
-    this.print_line(this.playerData[0].name,this.playerData[0].pos)
+    while(this.finish === false) {
 
+      for (var i = 0; i < this.playerData.length; i++){
+        let dadu = Dice.roll();
+
+        if (this.playerData[i].pos + dadu >= this.length){
+          this.playerData[i].pos = this.length-1
+          this.pemenang = this.playerData[i]
+          this.finish = true
+          break
+        }
+        else{
+          this.print_line(this.playerData[i].name,this.playerData[i].pos)
+          this.playerData[i].pos += Dice.roll()
+        }
+        }
+
+    }
+    for (var i = 0; i < this.playerData.length; i++){
+      this.print_line(this.playerData[i].name,this.playerData[i].pos)
+    }
+
+    console.log(this.finished());
     //proses berhenti disini sampai kondisi finish
  }
 
  //kondisi selesai finished n winer
   finished() {
-
+    return "The winner is player " + this.pemenang.name
  }
+
   winner() {
 
  }
@@ -53,11 +77,13 @@ class JSRacer {
     console.log("\x1B[2J")
   }
 }
+
 var test = new JSRacer(3,30)
 
 
 // test.print_line('a',0)
 // test.print_board()
 test.advanced_player()
+test.finished()
 
 export default JSRacer
