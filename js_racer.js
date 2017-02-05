4db0feb4457c26de2c091bf27880c80f05fe5601
danilags <aguspermadi94@gmail.com>
2017-02-05 14:53:00 +0700

    Daniel js_racer

--- js_racer.js
+++ js_racer.js
@@ -3,27 +3,105 @@
 import Dice from "./dice.js"
 
 class JSRacer {
-  constructor(players, length, sides) {
-
+  constructor(pemain, panjang) {
+    // isi arr['a', 'b', 'c']
+    this.pemain = pemain;
+    // 30
+    this.panjang = panjang;
+    // objek tempat pemain
+    this.player_object = {} 
+    this.player_object2 = [{
+      nama : "A", posisi : 0
+    }, {
+      nama : "B", posisi : 0
+    }, {
+      nama : "C", posisi : 0
+    }]
+    //this.samping = sides;
+    this.pemenang = false;
+    this.isFinish = false
+    this.namaPemenang = null
   }
+
+  positions() {
+    // create posisi awal - masukin ke objek
+    for(let i = 0; i < this.pemain.length; i++) {
+      this.player_object[this.pemain[i]] = 0
+
+    }
+    // console.log(this.player_object)
+  }
+
   print_board() {
+    
+    for(let i=0; i<this.pemain.length; i++) {
+      //asumsi i = 0
+      //let namaPemain = this.pemain[i]   //pemain[0] -> a
+      // let posisiPemain = this.player_object[namaPemain] //this.player_positions[a] -> 0
+      //this.player_object[namaPemain] = posisiPemain;
 
+      this.print_line(this.player_object2[i].nama, 0) // (nama. posisi)
+      
+    }
+    console.log("\n")
   }
-  print_line(player, pos) {
 
-  }
-  advanced_player(player) {
+  print_line(namaPemain, posisiPemain) {
+    let arr = [];
 
+    for(let i=0; i < this.panjang; i++) {
+      
+      if(posisiPemain == i) {
+        arr.push(namaPemain)  
+      } else {
+        arr.push(" ");
+      }
+    }
+    console.log(arr.join("|"))
   }
-  finished() {
 
+  advanced_player(){
+    this.print_board()
+    console.log("\n");
+
+    while(!this.pemenang){
+      // player = pemain
+      // playerData =  objekku
+      for(let i=0; i<this.pemain; i++){
+        if(!this.pemenang){
+          this.player_object2[i].posisi += Dice.roll()
+        }else{
+          this.player_object2[i].posisi = this.player_object2[i].posisi
+        }
+
+        if(this.player_object2[i].posisi >= this.panjang-1){
+          this.pemenang = true
+          this.namaPemenang = this.player_object2[i].nama
+          this.player_object2[i].posisi = this.panjang-1
+          this.print_line(this.player_object2[i].nama, this.player_object2[i].posisi)
+        }else{
+          this.print_line(this.player_object2[i].nama, this.player_object2[i].posisi)
+        }
+
+      }
+      console.log("\n");
+    }
+    this.winner()
   }
+
   winner() {
-
+    console.log(`The winner is ${this.namaPemenang}`);
   }
   reset_board() {
     console.log("\x1B[2J")
   }
+
 }
+let test = new JSRacer(3, 30);
+//test.positions()
+//test.print_board(false)
+// test.print_line()
+test.print_board()
+test.advanced_player()
 
 export default JSRacer
